2b222a1f820c96a79ee625a80d0ba91a21522c66
filipe <ftcarvalho@gmail.com>
2022-11-14 22:36:55 +0000

    algumas alterações

---
+++ frontend/frontend.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct utilizador utilizador {
+	char *username;
+	int saldo;
+}
+
+//comandos frontend
+// sell
+// list
+// licat
+// lisel
+// lival
+// litime
+// time
+// buy
+// cash
+// add
+// exit
